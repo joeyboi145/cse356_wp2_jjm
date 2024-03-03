09c07849ab64132c7891412e509f4da8ad7a6194
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-02 21:16:24 -0500

    Moves application code into new directory called 'application' and added README files

---
+++ application/index.js
@@ -0,0 +1,161 @@
+// Application Server
+
+const express = require('express')
+// const session = require("express-session")
+// const MongoDBSession = require('connect-mongodb-session')(session)
+const mongoose = require('mongoose')
+const mongoDB = 'mongodb://127.0.0.1:27017/wp2'
+const port = 8000
+
+mongoose.connect(mongoDB)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// const store = new MongoDBSession({
+//     uri: mongoDB,
+//     collection: 'sessions'
+// })
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+
+// app.use(
+//     session({
+//       name: 'session_name',
+//       secret: "supersecret difficult to guess string",
+//       cookie: {},
+//       resave: false,
+//       saveUninitialized: false,
+//       store: store
+//     })
+// )
+
+
+const User = require('./models/users')
+
+// ***=== POST Requests ===***
+// '/adduser'
+// '/verify'
+// '/login'
+// '/logout'
+
+app.post('/adduser', async (req, res) => {
+    console.log("'/adduser' POST request")
+    console.log(req.body);
+    const { username, password, email } = req.body;
+    res.append('X-CSE356', '65b99885c9f3cb0d090f2059');
+    let user = null;
+
+    try {
+        user = await User.findOne({ email });
+        if (user != null){
+            var message = "Duplicate email. Email must be unique"
+            res.status(400);
+            res.send({status: "ERROR", message: message})
+            return;
+        }
+        user = await User.findOne({ username })
+        if (user != null){
+            message = "Duplicate username. Username must be unique."
+            res.status(400);
+            res.send({status: "ERROR", message: message})
+            return;
+        }
+
+        let verify_key = parseInt(Math.random() * (999999 - 100000) + 100000);
+        user = new User({
+            username,
+            password,
+            email,
+            verify_key
+        });
+    
+        await user.save();
+        console.log(`NEW USER: ${username}`);
+        console.log(`VERIFICATION CODE: ${verify_key}\n`);
+        res.status(200);
+        res.send({ status: "OK", message: "Success! Please verify" });
+    } catch (err) { 
+        console.log(err);
+        res.status(500)
+        res.send({status: "ERROR", message: "Server Error"})
+    }
+});
+
+app.get('/verify/:email/:key', async (req, res) => {
+    console.log("'/verify' GET request")
+    let email = req.params.email
+    let key = req.params.key
+    console.log(`{${email}, ${key}}`)
+
+    try {
+        let user = await User.findOne({ email })
+        if (user != null){
+            let verified = user.get("verify")
+            let verify_key = user.get("verify_key");
+            if (verified){
+                var message = "User already verified"
+                res.status(400);
+                res.send({status: "ERROR", message: message})
+            } else if (verify_key != key) {
+                var message = "Incorrect verification key"
+                res.status(400);
+                res.send({status: "ERROR", message: message})
+            } else {
+                await User.updateOne({ email} , { verify: true });
+                console.log("USER VERFIED\n");
+                res.status(200);
+                res.send({status: "OK", message: "Verified"});
+            }
+        } else {
+            res.status(400);
+            res.send({status: "ERROR", message: "User not found"})
+        }
+    } catch (err) { console.log(err); }
+});
+
+app.post('/verify', async (req, res) => {
+    console.log("'/verify' POST request")
+    console.log(req.body)
+    const { email, key } = req.body;
+    res.append('X-CSE356', '65b99885c9f3cb0d090f2059');
+
+    try {
+        let user = await User.findOne({ email })
+        if (user != null){
+            let verified = user.get("verify")
+            let verify_key = user.get("verify_key");
+            if (verified){
+                var message = "User already verified"
+                res.status(400);
+                res.send({status: "ERROR", message: message})
+            } else if (verify_key != key) {
+                var message = "Incorrect verification key"
+                res.status(400);
+                res.send({status: "ERROR", message: message})
+            } else {
+                await User.updateOne({ email} , { verify: true });
+                console.log("USER VERFIED\n");
+                res.status(200);
+                res.send({status: "OK", message: "Verified"});
+            }
+        } else {
+            res.status(400);
+            res.send({status: "ERROR", message: "User not found"})
+        }
+    } catch (err) { console.log(err); }
+});
+
+const server = app.listen(port, () => {
+    console.log(`\napp listening on port ${port}\n`)
+  })
+
+process.on('SIGINT', () => {
+    if (db) {
+      db.close()
+        .then(() => {
+          server.close(() => console.log("\nServer closed. Database instance disconnected\n"))
+        })
+        .catch((err) => console.log(err))
+    }
+  })
