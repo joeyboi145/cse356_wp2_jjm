770c0ca4937569cc76a6ba48c72a2e07fbe41283
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-06 11:53:58 -0500

    remove html render

--- application/index.js
+++ application/index.js
@@ -182,7 +182,7 @@ app.post('/login', async (req, res) => {
             console.log("already logged in");
         }
         res.setHeader('content-type', 'text/html');
-        res.status(200).render(__dirname + "/html/index.html"); 
+        res.status(200).render(__dirname + "/html/index"); 
         console.log("index.html servered\n")
 
     } catch (err) { 
