08e001d857adbc5685632ecbb7365c842ef9504d
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-06 17:47:54 -0500

    changed to event listener

--- application/html/index.js
+++ application/html/index.js
@@ -56,7 +56,7 @@ async function get_display_image(){
     }
 }
 
-document.getElementById("wp2").addEventListener("onload", get_display_image);
+window.getElementById("wp2").addEventListener("load", get_display_image);
 
 console.log("this is working!")
 // get_display_image()
