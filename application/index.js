0b0c17fb2a0c4ae8cb47d062b7867235bbbd5d3a
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-06 10:40:13 -0500

    5

--- application/index.js
+++ application/index.js
@@ -226,11 +226,11 @@ app.get('/tiles/l:LAYER/:V/:H.jpg', async (req, res) => {
             console.log(`Sent in color: ${filepath}\n`);
         } else {
             const image = await jimp.read(filepath)
-            const grey_image = image.grayscale();
-            console.log(grey_image)
+            // const grey_image = image.grayscale();
+            // console.log(grey_image)
 
-            const base64_image = await grey_image.getBase64Async(grey_image.getMIME());
-            console.log(base64_image)
+            const base64_image = await image.getBase64Async(image.getMIME());
+            // console.log(base64_image)
             res.status(200).send(base64_image);
             console.log(`Sent in bw: ${filepath}\n`);
         }
