91bf4338eb010f2bbab99ed8e3f6a57e3e297eff
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-07 04:00:00 -0500

    f

--- application/index.js
+++ application/index.js
@@ -255,7 +255,7 @@ app.get('/', (req, res, next) => {
         // });
         //res.status(200).send({status: 'OK'})
         // res.setHeader("content-type", "text/html")
-        res.sendFile('html', {root: __dirname + '/'})
+        //res.sendFile('html', {root: __dirname + '/'})
         express.static(__dirname + "/html")(req, res, next);
     // } else res.send({status: "ERROR", message: "Not Logged in"});
 })
