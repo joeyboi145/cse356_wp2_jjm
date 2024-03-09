fa187ed9ff8b01801d0a8655f27fe03469ec2bf1
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-09 14:20:03 -0500

    commented out cookieParser

--- application/server.js
+++ application/server.js
@@ -32,7 +32,7 @@ app.use(cookieSession({
     domain: 'jrgroup.cse356.compas.cs.stonybrook.edu',
     path: '/'
 }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // app.use(
 //     session({
