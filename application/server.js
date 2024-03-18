4009b26baf46fb1932ec7b9e2e5f5fdb885d1b3b
Joe Martinez <joe.martinez@stonybrook.edu>
2024-03-18 15:48:26 -0400

    added domain and maxAge

--- application/server.js
+++ application/server.js
@@ -38,7 +38,9 @@ app.use(
     session({
         secret: "wp2 supersecret string",
         cookie: {
-            name: 'token'
+            name: 'token',
+            domain: 'jrgroup.cse356.compas.cs.stonybrook.edu',
+            maxAge: 24 * 60 * 60 * 1000
         },
         resave: true,
         saveUninitialized: true,
